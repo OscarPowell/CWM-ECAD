4e5890a76f516083617fc3f8f4f52a3ebd40f95c
Cloud User <centos@ip-172-31-3-78.eu-west-2.compute.internal>
2020-06-01 15:37:38 +0000

    Solution to Ex3. Success/Failure output doesn't print yet.

--- Ex3/top.v
+++ Ex3/top.v
@@ -20,11 +20,22 @@
 
 module counter(
     //Todo: add ports 
-
-    );
-                    
+    input rst,
+    input enable,
+    input direction,
+    input clk,
+    output [7:0] counter_out
+    );               
     //Todo: add registers and wires, if needed
+    reg [7:0] counter;
 
     //Todo: add user logic
-      
+    //first try and get it to work just with direction moving forward
+    assign counter_out = counter;
+	  
+    always@(posedge clk or posedge rst)
+	counter <= rst?0: //if rst true then it resets
+		enable?(direction?(counter+1):(counter-1) //if enable true it checks direction
+                        ):counter;			  //otherwise no change
+
 endmodule
