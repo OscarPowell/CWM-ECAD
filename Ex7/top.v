413f08c482a0d9d9d3b96a877fdf7c6ec43f65ab
Cloud User <centos@ip-172-31-3-78.eu-west-2.compute.internal>
2020-06-05 14:29:41 +0000

    Ex7, problems with result output

--- Ex7/top.v
+++ Ex7/top.v
@@ -18,50 +18,19 @@ module times_table(
 	input [2:0] b,
 	input enable,
 	output [5:0] result );
-
-	reg [4:0] address;
-	wire [5:0] ab;
 	
-	assign ab = {a,b};
-	always@(posedge clk) begin
-		case(ab)
-			6'b001001: address = 3'd1;
-			6'b010001: address = 3'd2;
-			6'b011001: address = 3'd3;
-			6'b100001: address = 3'd4;
-			6'b101001: address = 3'd5;
-			6'b110001: address = 3'd6;
-			6'b111001: address = 3'd7;
-			6'b010010: address = 3'd4;
-			6'b011010: address = 3'd6;
-			6'b100010: address = 3'd8;
-			6'b101010: address = 3'd10; //(below: addresses-pink, actual numbers-blue
-			6'b110010: address = 3'd11; //12
-			6'b111010: address = 3'd12; //14
-			6'b011011: address = 3'd9;
-			6'b100011: address = 3'd11; //12
-			6'b101011: address = 3'd13; //15
-			6'b110011: address = 3'd15; //18
-			6'b111011: address = 3'd17; //21
-			6'b110011: address = 3'd14; //16
-			6'b111011: address = 3'd16; //20
-			6'b110011: address = 3'd18; //24
-			6'b111011: address = 3'd20; //28
-			6'b110011: address = 3'd19; //25
-			6'b110011: address = 3'd21; //30
-			6'b111011: address = 3'd22; //35
-			6'b110011: address = 3'd23; //36
-			6'b111011: address = 3'd24; //42
-			6'b110011: address = 3'd25; //49
-		endcase
+	reg [5:0] address;	
+	
+	always @(a or b) begin
+		address = {a,b};
 	end
 
-	mybram bram (
-  .clka(clk),    // input wire clka
-  .ena(enable),      // input wire ena
-  .wea(0),      // input wire [0 : 0] wea
-  .addra(address),  // input wire [4 : 0] addra
-  .dina(6'b000000),    // input wire [5 : 0] dina
-  .douta(result[5:0])  // output wire [5 : 0] douta
+	blk_mem_gen_0 myBram (
+  .clka(clk),   		 // input wire clka
+  .ena(enable),     	 // input wire ena
+  .wea(1),      		 // input wire [0 : 0] wea
+  .addra(address[5:0]),  // input wire [5 : 0] addra
+  .dina(6'b000000),      // input wire [5 : 0] dina
+  .douta(result[5:0])    // output wire [5 : 0] douta
 );
 endmodule
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -8,5 +8,52 @@
 //////////////////////////////////////////////////////////////////////////////////
 module top_tb(  );
 
+	//Parameters
+	parameter TEST_PERIOD = 160; //sets how long test runs for
+	parameter CLK_PERIOD = 10; //clock period
+
+	//Registers and wires
+	reg clk;
+    reg [2:0] a;
+	reg [2:0] b;
+	reg err;
+	reg enable;
+	wire [5:0] result;
+
+	//Clock generation
+	initial begin
+        clk = 1'b0;
+        forever
+        	#(CLK_PERIOD/2) clk=~clk;
+    end
+
+	//Logic to initialise variables and check the device works
+	initial begin
+		err <= 0;
+		enable <= 1;
+		a = 3'b001;
+		b = 3'b001;
+		#5
+		if(result != 2'd49) begin
+			$display("***TEST FAILED! Answer was %b, should be 49 ***",result);
+			err = 1;
+		end
+	end	
+
+	//Finish test, check for success
+	initial begin
+		#TEST_PERIOD
+		if (err==0)
+        	$display("***TEST PASSED! :) ***");
+		$finish;
+	end
+
+	//Instantiate device module
+	times_table top(
+		.clk (clk),
+		.a (a[2:0]),
+		.b (b[2:0]),
+		.enable (enable),
+		.result (result[5:0]) );
 
 endmodule
